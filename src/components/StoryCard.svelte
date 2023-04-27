8b88d8b31b39f674eda8719b2ab6c888151ceb05
jackbisceglia <jackbisceglia2000@gmail.com>
2023-04-27 17:57:36 -0400

    fix: load state, tone down animation

--- src/components/StoryCard.svelte
+++ src/components/StoryCard.svelte
@@ -8,29 +8,29 @@
 </script>
 
 <div
-	class="text-neutral-200 w-full flex my-4 justify-start h-full gap-2 p-5 transition-all duration-300 ease-in-out border-2 rounded-lg shadow-lg group hover:my-6 hover:scale-110 bg-coolmint-700 hover:bg-coolmint-700/20 hover:border-coolmint-700 border-coolmint-700"
+	class="flex justify-start w-full h-full gap-2 p-5 my-4 transition-all duration-300 ease-in-out border-2 rounded-lg shadow-lg text-neutral-200 group hover:my-5 hover:scale-[101.5%] bg-coolmint-700 hover:bg-coolmint-700/20 hover:border-coolmint-700 border-coolmint-700"
 >
 	<p class="text-lg font-bold">{rank}.</p>
 	<div class="flex flex-col gap-2">
-		<div class="top flex gap-2 items-start flex-col md:flex-row md:items-center">
+		<div class="flex flex-col items-start gap-2 top md:flex-row md:items-center">
 			<a
 				href={`/item?id=${story.id}`}
-				class="text-lg sm:text-xl font-bold hover:underline self-baseline break-all line-clamp-1"
+				class="text-lg font-bold break-all sm:text-xl hover:underline self-baseline line-clamp-1"
 				>{story.title}</a
 			>
 			{#if story.url}
 				<a
 					target="_blank"
 					href={story.url}
-					class="font-light text-coolmint-400 hover:text-coolmint-500 transition-colors duration-100 italic text-sm break-all line-clamp-1"
+					class="text-sm italic font-light break-all transition-colors duration-100 text-coolmint-400 hover:text-coolmint-500 line-clamp-1"
 					>({story.url})</a
 				>
 			{/if}
 		</div>
-		<div class="bottom flex flex-col text-coolmint-400">
+		<div class="flex flex-col bottom text-coolmint-400">
 			<p><span class=" text-neutral-200">{story.score}</span> points</p>
 			<p><span class=" text-neutral-200">{story.descendants}</span> comments</p>
-			<p class="text-coolmint-400 font-medium">
+			<p class="font-medium text-coolmint-400">
 				posted by:
 				{story.by}
 				{getFormattedTimeSince(story.time_posted)} hr ago
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,22 +20,8 @@
 </script>
 
 <script lang="ts">
+	import Spinner from '../components/Spinner.svelte';
 	import StoryCard from '../components/StoryCard.svelte';
-
-	// const genMockPost = (order: number): Story => {
-	// 	console.log(order);
-	// 	return {
-	// 		id: crypto.randomUUID(),
-	// 		rank: order,
-	// 		title: (Math.random() + 1).toString(36).substring(2),
-	// 		hn_href: `/article/10`,
-	// 		external_href: 'https://jackbisceglia.com/',
-	// 		points: Math.floor(Math.random() * 1000),
-	// 		author: 'Jack Bisceglia',
-	// 		time_posted: 1000 * 60 * 60,
-	// 		num_comments: Math.floor(Math.random() * 1000)
-	// 	};
-	// };
 
 	const STATE: StateType = {
 		num_stories: 10,
@@ -76,18 +62,16 @@
 	})();
 </script>
 
-<!-- <h1 class="text-5xl font-bold text-coolmint-600">Hacker News</h1> -->
-
 {#if !STATE.loading}
 	{#each STATE.stories as p, index}
 		<StoryCard story={p} rank={index + 1} />
 	{/each}
 	<button
 		on:click={handleFetchNextPage}
-		class="flex mx-auto bg-coolmint-700 rounded-md font-bold items-center gap-1 px-6 py-1 my-1 transition-all text-coolmint-400 duration-200 ease-in-out flex-start hover:bg-coolmint-500 hover:text-coolmint-800"
+		class="flex items-center gap-1 px-6 py-1 mx-auto my-1 font-bold transition-all duration-200 ease-in-out rounded-md bg-coolmint-700 text-coolmint-400 flex-start hover:bg-coolmint-500 hover:text-coolmint-800"
 	>
 		Load More
 	</button>
 {:else}
-	<p>Loading</p>
+	<Spinner />
 {/if}
